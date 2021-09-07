0b3c710cf5d556acb9268975f31052cd61c834e9
Trivita Annisa Utami <trivita1900018074@webmail.uad.ac.id>
2021-09-08 00:13:10 +0700

    Pong Game

---
+++ Assets/SideWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    public PlayerControl player;
+    [SerializeField]
+    private GameManager gameManager;
+
+    void OnTriggerEnter2D(Collider2D anotherCollider){
+        if(anotherCollider.name == "Ball"){
+            player.IncrementScore();
+            if(player.Score < gameManager.maxScore){
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
